1a480e374e5400a0c7d9dfd02f9ffef2a77c327e
Artsiom Trubchyk <aglitchman@gmail.com>
2025-03-23 22:13:21 +0300

    CI and better example

---
+++ example/example_data.lua
@@ -0,0 +1,229 @@
+return [[
+[
+  {
+    "_id": "67e05ce0a14aa938cdae1aa9",
+    "index": 0,
+    "guid": "538b6762-e231-447e-9e47-eaf1444a053e",
+    "isActive": true,
+    "balance": "$2,370.77",
+    "picture": "http://placehold.it/32x32",
+    "age": 28,
+    "eyeColor": "blue",
+    "name": "Dianna Dorsey",
+    "gender": "female",
+    "company": "MYOPIUM",
+    "email": "diannadorsey@myopium.com",
+    "phone": "+1 (847) 580-2640",
+    "address": "706 Ashford Street, Mansfield, Georgia, 4786",
+    "about": "Ad ullamco pariatur cupidatat dolore. Incididunt officia fugiat veniam duis aliqua commodo amet voluptate sint. Duis voluptate commodo exercitation excepteur commodo. Id proident officia minim labore velit eiusmod sit aute esse sunt consectetur. Irure laborum nulla mollit eu dolor.\r\n",
+    "registered": "2019-03-29T08:46:44 -03:00",
+    "latitude": 57.445613,
+    "longitude": 63.68796,
+    "tags": [
+      "dolore",
+      "elit",
+      "dolore",
+      "enim",
+      "exercitation",
+      "commodo",
+      "ex"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Kristie Wright"
+      },
+      {
+        "id": 1,
+        "name": "Christa Stephens"
+      },
+      {
+        "id": 2,
+        "name": "Kathie Trevino"
+      }
+    ],
+    "greeting": "Hello, Dianna Dorsey! You have 4 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "67e05ce0e1c85f3afb17b9d2",
+    "index": 1,
+    "guid": "1d5d6aaf-788d-437d-9e45-c86b1dfd57c2",
+    "isActive": false,
+    "balance": "$2,915.22",
+    "picture": "http://placehold.it/32x32",
+    "age": 22,
+    "eyeColor": "brown",
+    "name": "Carolina Cleveland",
+    "gender": "female",
+    "company": "ZAPPIX",
+    "email": "carolinacleveland@zappix.com",
+    "phone": "+1 (849) 555-2071",
+    "address": "888 Polar Street, Hoagland, Arkansas, 9457",
+    "about": "Cupidatat est tempor laborum fugiat irure sunt quis est fugiat incididunt do et quis. Tempor anim qui veniam id in reprehenderit cillum tempor esse irure qui deserunt veniam. Cupidatat culpa consequat ea fugiat anim aliquip consectetur anim ea excepteur. Excepteur dolor veniam eiusmod duis ea fugiat sunt sint do. Culpa mollit sint reprehenderit proident eu eu eu quis consectetur tempor cupidatat esse duis.\r\n",
+    "registered": "2018-11-01T12:32:06 -03:00",
+    "latitude": 1.304018,
+    "longitude": 19.799182,
+    "tags": [
+      "enim",
+      "non",
+      "eu",
+      "ex",
+      "sunt",
+      "ad",
+      "Lorem"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Romero England"
+      },
+      {
+        "id": 1,
+        "name": "Pansy Fields"
+      },
+      {
+        "id": 2,
+        "name": "Jane Bryant"
+      }
+    ],
+    "greeting": "Hello, Carolina Cleveland! You have 2 unread messages.",
+    "favoriteFruit": "apple"
+  },
+  {
+    "_id": "67e05ce0b36a481a3b5f4a43",
+    "index": 2,
+    "guid": "917ed364-5ff0-4ea7-882d-6f6ea2936ca4",
+    "isActive": true,
+    "balance": "$2,405.45",
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "eyeColor": "brown",
+    "name": "Herminia Burks",
+    "gender": "female",
+    "company": "VETRON",
+    "email": "herminiaburks@vetron.com",
+    "phone": "+1 (824) 516-2043",
+    "address": "680 Douglass Street, Coalmont, Virginia, 533",
+    "about": "Commodo occaecat excepteur non cupidatat incididunt anim eu magna anim nulla magna id irure. Ut fugiat Lorem ex aliqua fugiat aliqua do dolor. Nisi est nostrud voluptate elit cupidatat duis ullamco. Culpa Lorem amet veniam aliqua.\r\n",
+    "registered": "2022-05-10T07:05:31 -03:00",
+    "latitude": -62.19312,
+    "longitude": 170.347755,
+    "tags": [
+      "ea",
+      "elit",
+      "cillum",
+      "non",
+      "deserunt",
+      "ea",
+      "dolore"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Robinson Valentine"
+      },
+      {
+        "id": 1,
+        "name": "Mallory Alford"
+      },
+      {
+        "id": 2,
+        "name": "Olive Mann"
+      }
+    ],
+    "greeting": "Hello, Herminia Burks! You have 7 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "67e05ce05faa12d989cb1fd7",
+    "index": 3,
+    "guid": "9035b466-348f-4a45-8a4e-7cd4ca50cc50",
+    "isActive": true,
+    "balance": "$1,965.90",
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "eyeColor": "green",
+    "name": "Rutledge Buckley",
+    "gender": "male",
+    "company": "ZENSUS",
+    "email": "rutledgebuckley@zensus.com",
+    "phone": "+1 (977) 476-2046",
+    "address": "914 Hinckley Place, Fresno, Wyoming, 764",
+    "about": "Ex magna laborum duis ex quis excepteur deserunt cillum. Non incididunt culpa ea ad esse nostrud. Eiusmod irure est excepteur commodo in nisi consectetur. Enim in mollit cupidatat consequat minim dolor cupidatat do laboris sunt.\r\n",
+    "registered": "2021-10-04T03:32:18 -03:00",
+    "latitude": 80.871084,
+    "longitude": -166.454018,
+    "tags": [
+      "in",
+      "sit",
+      "culpa",
+      "cupidatat",
+      "ipsum",
+      "laboris",
+      "anim"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Samantha Douglas"
+      },
+      {
+        "id": 1,
+        "name": "Gretchen Moore"
+      },
+      {
+        "id": 2,
+        "name": "Buckley Ruiz"
+      }
+    ],
+    "greeting": "Hello, Rutledge Buckley! You have 8 unread messages.",
+    "favoriteFruit": "banana"
+  },
+  {
+    "_id": "67e05ce09a8a28f3f6ccd9ff",
+    "index": 4,
+    "guid": "f51cb9f2-adf2-4a76-ac1a-60c2bf3ed321",
+    "isActive": false,
+    "balance": "$1,173.31",
+    "picture": "http://placehold.it/32x32",
+    "age": 33,
+    "eyeColor": "brown",
+    "name": "Molly Ryan",
+    "gender": "female",
+    "company": "ASSITIA",
+    "email": "mollyryan@assitia.com",
+    "phone": "+1 (893) 519-3106",
+    "address": "310 Lorraine Street, Cowiche, Florida, 7568",
+    "about": "Est incididunt est aliqua laboris qui. Eiusmod esse minim adipisicing deserunt occaecat. Adipisicing labore commodo elit sit consectetur excepteur.\r\n",
+    "registered": "2023-01-21T01:36:57 -03:00",
+    "latitude": -28.686936,
+    "longitude": -135.826185,
+    "tags": [
+      "occaecat",
+      "ea",
+      "ea",
+      "voluptate",
+      "irure",
+      "enim",
+      "aliquip"
+    ],
+    "friends": [
+      {
+        "id": 0,
+        "name": "Mollie Hopkins"
+      },
+      {
+        "id": 1,
+        "name": "Reynolds Holman"
+      },
+      {
+        "id": 2,
+        "name": "Best Langley"
+      }
+    ],
+    "greeting": "Hello, Molly Ryan! You have 8 unread messages.",
+    "favoriteFruit": "banana"
+  }
+]
+    ]]
